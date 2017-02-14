bb80b8a5009182be319ad24756a74d7ba3760a8e
Comp <Comp@172.22.226.133>
2017-02-14 16:33:04 -0600

    Added line break before Hello World

--- src/demo2/Demo2.java
+++ src/demo2/Demo2.java
@@ -15,6 +15,8 @@ public class Demo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println();
+        
         System.out.println("Hello World!");
     }
     
