02b821ae51acb9f8e8f87a8e3e8a11df60d2534c
Comp <Comp@172.22.226.133>
2017-02-14 16:30:59 -0600

    Initial Version

---
+++ src/demo2/Demo2.java
@@ -0,0 +1,21 @@
+/*
+ * Michael Fisher
+ * UW-Whitewater
+ */
+package demo2;
+
+/**
+ *
+ * @author Comp
+ */
+public class Demo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
